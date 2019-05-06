0a60b8eaf2d9427fc50621fc57cd923c1295286c
Prashanth Puranik <puranik@digdeeper.in>
2019-05-06 15:52:47 +0530

    completed const

--- puranik3/01-es2015/02-const.js
+++ puranik3/01-es2015/02-const.js
@@ -1,2 +1,21 @@
 const x = 1;
-x = 2; // error!
+// x = 2; // error!
+
+const john = {
+    name: 'John',
+    age: 32
+};
+
+// john = { // error
+//     name: 'Jonathan',
+//     age: 32
+// };
+
+john.name = 'Jonathan';
+console.log( john );
+
+const days = [ 'Sunday', 'Monday' ];
+days[2] = 'Tuesday';
+console.log( days );
+
+days = [ 'Sunday', 'Monday', 'Tuesday' ];
